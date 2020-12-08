42b16de77008bba27f63af912e15aa93b5d8e83c
Mikołaj Lenczewski <mikolaj.lenczewski308@gmail.com>
2020-12-08 17:03:55 +0000

    Actually added the 00-keyboard.conf file

--- deploy.sh
+++ deploy.sh
@@ -37,9 +37,10 @@ mv /etc/X11/xinit/xinitrc /etc/X11/xinit/xinitrc.bak
 cp .sysconfig/xinitrc /etc/X11/xinit/
 
 XORG_CONF="/etc/X11/xorg.conf.d"
+[ -d ${XORG_CONF} ] || mkdir ${XORG_CONF}
 [ -f ${XORG_CONF}/00-keyboard.conf ] \
 	&&mv ${XORG_CONF}/00-keyboard.conf ${XORG_CONF}/00-keyboard.conf.bak
-cp .sysconfig/00-keyboard.conf ${XORG_CONF}
+cp .sysconfig/00-keyboard.conf ${XORG_CONF}/00-keyboard.conf
 
 mv /etc/ssh/ssh_config /etc/ssh/ssh_config.bak
 cp .sysconfig/ssh_config /etc/ssh/
